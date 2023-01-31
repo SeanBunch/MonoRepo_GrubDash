7e87fc6fd5c67d3b18254592be8d050485eb7b00
Sean Bunch <100799798+SeanBunch@users.noreply.github.com>
2023-01-30 23:49:03 -0500

    server file updated app const

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,8 +8,6 @@ const dishesRouter = require("./dishes/dishes.router");
 
 const app = express();
 
-// You have not learned about CORS yet.
-// The following line let's this API be used by any website.
 app.use(cors());
 app.use(express.json());
 
