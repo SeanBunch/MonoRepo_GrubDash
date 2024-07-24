2308b75535aafdd3a40ac3ac0e108639f5ae0b5a
Sean Bunch <databaws@gmail.com>
2024-07-24 15:24:38 -0400

    DishEdit migrated to RTKQ old code saved

--- frontend/src/dishes/DishEdit.tsx
+++ frontend/src/dishes/DishEdit.tsx
@@ -1,43 +1,51 @@
-import React, { useEffect, useState } from "react";
-import { useHistory, useParams } from "react-router-dom";
-import ErrorAlert from "../layout/ErrorAlert";
+// import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+// import ErrorAlert from "../layout/ErrorAlert";
 import DishForm from "./DishForm";
-import { readDish, updateDish } from "../utils/api";
-import { Dish, RouteParams } from "../types/types";
+// import { readDish, updateDish, useReadDishQuery, useUpdateDishMutation } from "../utils/api";
+import { RouteParams } from "../types/types";
+
+import { useReadDishQuery } from "../utils/api";
+import React from "react";
 
 function DishEdit() {
-  const history = useHistory();
-  const { dishId } = useParams<RouteParams>();
+  // const history = useHistory();
+  // const { dishId } = useParams<RouteParams>();
 
-  const [dish, setDish] = useState<Dish | null>(null);
-  const [error, setError] = useState(null);
+  // const [dish, setDish] = useState<Dish | null>(null);
+  // const [error, setError] = useState(null);
 
-  useEffect(() => {
-    const abortController = new AbortController();
-    readDish(dishId, abortController.signal).then(setDish).catch(setError);
-    return () => abortController.abort();
-  }, [dishId]);
+  // useEffect(() => {
+  //   const abortController = new AbortController();
+  //   readDish(dishId, abortController.signal).then(setDish).catch(setError);
+  //   return () => abortController.abort();
+  // }, [dishId]);
+// ===========================================================================
+// ===========================================================================
+// ===========================================================================
+const { dishId } = useParams<RouteParams>();
+const { data: dish, error } = useReadDishQuery(dishId);
 
-  function submitHandler(updatedDishOrder: Dish) {
-    const abortController = new AbortController();
+// ===========================================================================
+// ===========================================================================
+// ===========================================================================
+
+  // function submitHandler(updatedDishOrder: Dish) {
+  //   const abortController = new AbortController();
   
-    updateDish(updatedDishOrder, abortController.signal)
-      .then(() => history.push(`/dashboard`))
-      .catch(setError);
+  //   updateDish(updatedDishOrder, abortController.signal)
+  //     .then(() => history.push(`/dashboard`))
+  //     .catch(setError);
 
-    return () => abortController.abort();
-  }
+  //   return () => abortController.abort();
+  // }
 
-  function cancelHandler() {
-    history.goBack();
-  }
+  // function cancelHandler() {
+  //   history.goBack();
+  // }
 
   const child = dish ? (
-    <DishForm
-      initialState={dish}
-      onCancel={cancelHandler}
-      onSubmit={submitHandler}
-    />
+    <DishForm initialState={dish}/>
   ) : (
     <p>Loading...</p>
   );
@@ -45,7 +53,12 @@ function DishEdit() {
   return (
     <main>
       <h1>Edit Dish</h1>
-      <ErrorAlert error={error} />
+      {
+
+error && "status" in error ? <div className="alert alert-danger m-2">Error: {error.status}</div> : null
+
+}
+      {/* <ErrorAlert error={error} /> */}
       {child}
     </main>
   );
