015f0b12a3e9c7e73dc0a20d9fe60bef5411679d
Sean Bunch <databaws@gmail.com>
2024-07-24 15:21:39 -0400

    store.ts exported

--- frontend/src/store.ts
+++ frontend/src/store.ts
@@ -1,13 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { api } from "./utils/api";
 
-export const store = configureStore({
+const store = configureStore({
     reducer: {
         [api.reducerPath]: api.reducer,
     },
     middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware().concat(api.middleware),
 });
-
+export default store;
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
